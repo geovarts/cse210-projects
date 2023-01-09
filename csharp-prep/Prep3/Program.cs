84506ac9421628344962147a17962a710a183126
geovarts <luisartes765@gmail.com>
2023-01-08 23:49:13 -0500

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random number_random = new Random();
+        int magic_number = number_random.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magic_number){
+            Console.Write("What is your guess? ");
+            string s_guess = Console.ReadLine();
+            guess = int.Parse(s_guess);
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Excellent, you guessed it");
+            }
+
+        }
     }
 }
