da4dccb41875b28069d2765e0e511cce5d222a7a
geovarts <luisartes765@gmail.com>
2023-01-16 00:02:51 -0500

    Finished Acivity Week2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        //job1.ShowTitleDisplay();
+
+        Job job2 = new Job();
+        job2._company = "Manager";
+        job2._jobTitle = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+
+        Resume resume = new Resume();
+        resume._name = "Geovanny Artes";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.ResumeDisplay();
     }
 }
